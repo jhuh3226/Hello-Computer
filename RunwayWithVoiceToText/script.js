022cc46b1fef03f66c9ac1e3594a0add3363ffb9
jhuh3226 <jhuh3226@gmail.com>
2021-10-10 12:33:41 -0400

    Before changing code

--- RunwayWithVoiceToText/script.js
+++ RunwayWithVoiceToText/script.js
@@ -1,4 +1,6 @@
 const SpeechRecognition = webkitSpeechRecognition; //eslint-disable-line
+const gallery = document.getElementById('gallery');
+const imageIndexs = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 const model = new rw.HostedModel({
     url: "https://attngan-63a75f28.hosted-models.runwayml.cloud/v1/",
@@ -37,37 +39,47 @@ const generate = (phrase) => {
     };
     model.query(inputs).then(outputs => {
         const { result } = outputs;
-        // use the outputs in your project
+
+        // imageIndexs.forEach(i => {
         const img = document.createElement('img');
         img.src = result;
-        document.getElementById('results').prepend(img);    // Adding AI imagined image
+        img.alt = phrase;
+        img.classList.add('galleryImg');
+        memory.appendChild(img);
+        // gallery.appendChild(img);
+        // });
+
+        // use the outputs in your project
+        // const img = document.createElement('img');
+        // img.src = result;
+        // document.getElementById('results').prepend(img);    // Adding AI imagined image
 
         // Adding date stamp
         var date = randomDate('01/01/2021', '02/26/1993');  // Change the latest date as today
         var convertedDate = document.createTextNode(date);
-        document.getElementById('date-div').prepend(convertedDate);
+        document.getElementById('dateStamp').appendChild(convertedDate);
 
         // Adding alt text
-        document.getElementById('text-div').prepend(phrase);
+        // document.getElementById('text-div').appendChild(phrase);
     });
 };
 
 // Create random date between two period
 // https://stackoverflow.com/questions/31378526/generate-random-date-between-two-dates-and-times-in-javascript?lq=1
 function randomDate(date1, date2) {
-	function randomValueBetween(min, max) {
-		return Math.random() * (max - min) + min;
-	}
-	var date1 = date1 || '01-01-1970'
-	var date2 = date2 || new Date().toLocaleDateString()
-	date1 = new Date(date1).getTime()
-	date2 = new Date(date2).getTime()
-	if (date1 > date2) {
-		return new Date(randomValueBetween(date2, date1)).toLocaleDateString()
-	} else {
-		return new Date(randomValueBetween(date1, date2)).toLocaleDateString()
+    function randomValueBetween(min, max) {
+        return Math.random() * (max - min) + min;
+    }
+    var date1 = date1 || '01-01-1970'
+    var date2 = date2 || new Date().toLocaleDateString()
+    date1 = new Date(date1).getTime()
+    date2 = new Date(date2).getTime()
+    if (date1 > date2) {
+        return new Date(randomValueBetween(date2, date1)).toLocaleDateString()
+    } else {
+        return new Date(randomValueBetween(date1, date2)).toLocaleDateString()
 
-	}
+    }
 }
 
 //// You can use the info() method to see what type of input object the model expects
